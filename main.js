2194e3db82d085eec7f7596c820cb5df1733e58e
alexis1955 <fviguest@fvi.edu>
2016-07-15 13:08:41 -0400

    score

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ var mainState = {
       game.physics.arcade.enable(this.bird)
       this.bird.body.gravity.y = 1000
 
-      var spaceKey = game.input.keyboard.addKey( 
+      var spaceKey = game.input.keyboard.addKey(
                 Phaser.Keyboard.SPACEBAR)
 
       spaceKey.onDown.add(this.jump, this)
@@ -28,6 +28,9 @@ var mainState = {
 
       this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
 
+      this.score = 0;
+      this.labelScore = game.add.text(20, 20, "0",
+          { font: "30px Arial", fill: "#ffffff" });
 
     },
 
@@ -68,6 +71,9 @@ var mainState = {
         for (var i = 0; i < 8; i++)
             if (i != hole && i != hole + 1 && i!=hole+2)
                 this.addOnePipe(400, i * 60 + 10);
+
+                this.score += 1;
+                this.labelScore.text = this.score;  
     },
 };
 

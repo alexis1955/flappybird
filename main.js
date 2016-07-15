ab30d989f360f8d4edb5da7d7211713eca692e23
alexis1955 <fviguest@fvi.edu>
2016-07-15 12:02:33 -0400

    added pipes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 var mainState = {
     preload: function() {
       game.load.image('bird', 'assets/bird.png')
+      game.load.image('pipe', 'assets/pipe.png');
     },
 
     create: function() {
@@ -17,22 +18,51 @@ var mainState = {
                       Phaser.Keyboard.SPACEBAR);
 
       spaceKey.onDown.add(this.jump, this);
+
+      this.pipes = game.add.group();
+
+      this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
     },
 
     update: function() {
       if (this.bird.y < 0 || this.bird.y > 490){
         this.restartGame();
-      }
+    },
+        jump: function() {
+    this.bird.body.velocity.y = -350;
+    },
+    restartGame: function() {
+    // Start the 'main' state, which restarts the game
+    game.state.start('main');
     },
 
-    // jump: function() {
-    //   this.bird.body.velocity.y = -350;
-    // },
-    //
-    // restartGame: function() {
-    //   game.state.start('main');
-    // },
-};
+    addOnePipe: function(x, y) {
+      var pipe = game.add.sprite(x, y, 'pipe');
+      this.pipes.add(pipe);
+      game.physics.arcade.enable(pipe);
+      pipe.body.velocity.x = -200;
+
+      pipe.checkWorldBounds = true;
+      pipe.outOfBoundsKill = true;
+   },
+
+   addRowOfPipes: function() {
+    var hole = Math.floor(Math.random() * 5) + 1;
+
+    for (var i = 0; i < 8; i++)
+        if (i != hole && i != hole + 1)
+            this.addOnePipe(400, i * 60 + 10);
+},
+
+
+
+
+
+
+
+
+
+
 
 // Initialize Phaser, and create a 400px by 490px game
 var game = new Phaser.Game(400, 490);
